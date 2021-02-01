f51048ed63b1ea76b3e45d40ed736a90523b3a6b
ariel_rio_sie <ariel.rios@studioav.com.mx>
2021-02-01 12:18:51 -0600

    [FIX] Se corrige error de muestreo del postwork 6

--- Sesion_6/postwork_s6/postwork_s6.R
+++ Sesion_6/postwork_s6/postwork_s6.R
@@ -30,11 +30,29 @@ md <- summarise(md, goles.promedio = mean(sumagoles))
 typeof(md) # Es para ver la salida de nuestro datos (Es una lista)
 
 # Convertimos nuestra lista a un dataframe.
-md <- as.data.frame(md) 
+md <- as.data.frame(md)
+
+# Obtenemos el indice hasta la fecha de 12 de diciembre.
+indexDiciembre2019 <- which(md$xmonth == "2019-12")
+
+# Obtenemos los meses hasta la fecha del indice
+md1219 <- md[1:indexDiciembre2019, ]
 
 # Sacamos nuestros datos de nuestra serie de tiempo.
-prom.goles <- ts(md$goles.promedio, start = 1, frequency = 2)
+prom.goles <- ts(md1219$goles.promedio, start = 1, frequency = 10)
 
 # Graficamos nuestra serie de tiempo
 # Inicia la grafica desde agosto del 2010
-ts.plot(prom.goles)
+ts.plot(prom.goles) # Obtenemos un outlier del mes de agosto del 2013.
+
+# Eliminamos el valor de nuestros datos para tener una frecuencia de entre año y año de 10 meses
+md1219 <- filter(md1219, md1219$xmonth != "2013-06")
+
+dim(md1219) # Verificamos las dimensiones del df
+
+# Sacamos nuestros datos de nuestra serie de tiempo.
+prom.goles <- ts(md1219$goles.promedio, start = 1, frequency = 10)
+
+# Graficamos nuestra serie de tiempo
+# Inicia la grafica desde agosto del 2010
+ts.plot(prom.goles) # Ahora tenemos una frecuencia de 10
